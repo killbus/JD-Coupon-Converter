c0c1100bf99e022a9929ba8d7e64bde02ef3b02a
fy1128 <l.fuying@yahoo.com>
2016-09-19 16:29:06 +0800

    Updated and finished.

--- src/js/generate.js
+++ src/js/generate.js
@@ -1,9 +1,10 @@
 (function() {
 	$(function() {
-		var generate, input, output, key, roleid, to;
+		var generate, input, output, key, roleid, to, platform, warning_body;
 		output = $("#output");
 		input = $("#input");
 		generate = $("#generate");
+		warning_body = $(".modal-body");
 		
 		var request = {
 			QueryString : function(val)
@@ -15,13 +16,29 @@
 		}
 
 		generate.on("click", function(event) {
+			output.val('');
 			key = request.QueryString('key') ? request.QueryString('key') : request.QueryString('keyid');
 			roleid = request.QueryString('roleid');	
 			to = request.QueryString('to') ? request.QueryString('to') : request.QueryString('rul');
+			if (!key) {
+				warning_body.html('key 不能为空，请检查。');
+				$('.warning').modal();
+			}
+			if (!roleid) {
+				warning_body.html('roleid 不能为空，请检查。');
+				$('.warning').modal();
+			}
+			if (!to) {
+				to = 'jd.com';
+			}
+			platform = {'pc':'http://coupon.jd.com/ilink/couponSendFront/send_index.action?key='+key+'&roleId='+roleid+'&to='+to+'&', 'm':'http://coupon.m.jd.com/coupons/show.action?key='+key+'&roleId='+roleid+'&to='+'&', 'wq':'http://wqs.jd.com/promote/2016/getcoupon/index.html?keyid='+key+'&roleid='+roleid+'&rurl='+to+'&'};
 			
-			console.log(event);
-			console.log(key);
-			console.log(roleid);
+			if (key && roleid && to) {	
+				$.each(platform, function(k, v) {
+					output.val(!output.val() ? v : output.val()+'\n'+v);
+					console.log(v);
+				});
+			}
 		});
 	});
 }).call(this);
